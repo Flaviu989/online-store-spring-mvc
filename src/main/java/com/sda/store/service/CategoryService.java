fe77e73c773000e1c0e95510bd8d5a24d198f2a4
Flaviu989 <madomeg@yahoo.com>
2020-02-11 22:21:39 +0200

    Updated Category Service and Repository

--- src/main/java/com/sda/store/service/CategoryService.java
+++ src/main/java/com/sda/store/service/CategoryService.java
@@ -1,5 +1,11 @@
 package com.sda.store.service;
 
+import java.util.List;
+
+import com.sda.store.model.Category;
+
 public interface CategoryService {
+
+	List<Category> findMainCategories();
 
 }
--- src/main/java/com/sda/store/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/sda/store/service/implementation/CategoryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sda.store.service.implementation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sda.store.model.Category;
 import com.sda.store.repository.CategoryRepository;
 import com.sda.store.service.CategoryService;
 
@@ -11,4 +14,9 @@ public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Override
+	public List<Category> findMainCategories() {
+		return categoryRepository.findBySuperCategoryNull();
+	}
 }
