b4ad2d7ad29e2eac6fdf6ed66a4022653e7e8e1a
Flaviu989 <madomeg@yahoo.com>
2020-02-19 23:50:46 +0200

    Updated Welcome page

--- src/main/java/com/sda/store/controller/WelcomeController.java
+++ src/main/java/com/sda/store/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.sda.store.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sda.store.model.Category;
 import com.sda.store.service.CategoryService;
+import com.sda.store.service.ProductService;
 
 @Controller
 public class WelcomeController {
@@ -16,10 +18,15 @@ public class WelcomeController {
 	@Autowired
 	private CategoryService categoryService;
 
+	@Autowired
+	private ProductService productService;
+
 	@GetMapping("/")
-	public String displayLandingPage(Model model) {
+	public String displayLandingPage(Model model, Principal user) {
 		List<Category> mainCategory = categoryService.findMainCategories();
 		model.addAttribute("mainCategory", mainCategory);
+		if (user != null)
+			model.addAttribute("userName", user.getName());
 		return "welcome";
 	}
 
