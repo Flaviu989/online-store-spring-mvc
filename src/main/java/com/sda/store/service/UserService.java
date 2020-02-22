173713661245fe4709155396f558e9d00b9f73f4
Flaviu989 <madomeg@yahoo.com>
2020-02-22 13:58:33 +0200

    Updated User Service

--- src/main/java/com/sda/store/service/UserService.java
+++ src/main/java/com/sda/store/service/UserService.java
@@ -1,5 +1,11 @@
 package com.sda.store.service;
 
+import java.util.List;
+
+import com.sda.store.model.User;
+
 public interface UserService {
+
+	List<User> findAllUsers();
 
 }
--- src/main/java/com/sda/store/service/implementation/UserServiceImpl.java
+++ src/main/java/com/sda/store/service/implementation/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sda.store.service.implementation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sda.store.model.User;
 import com.sda.store.repository.UserRepository;
 import com.sda.store.service.UserService;
 
@@ -11,4 +14,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Override
+	public List<User> findAllUsers() {
+		return userRepository.findAll();
+	}
 }
