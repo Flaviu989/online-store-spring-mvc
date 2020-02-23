9c1c545859099d0b350556d7213288a87247a79d
Flaviu989 <madomeg@yahoo.com>
2020-02-23 11:45:10 +0200

    Updated User Controller and register form

--- src/main/java/com/sda/store/controller/UserController.java
+++ src/main/java/com/sda/store/controller/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sda.store.model.User;
 import com.sda.store.service.UserService;
 
+@RequestMapping("/register")
 @Controller
 public class UserController {
 
@@ -20,13 +22,13 @@ public class UserController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@GetMapping(value = "/registerForm")
+	@GetMapping("/form")
 	public String registerForm(Model model) {
 		model.addAttribute("user", new User());
 		return "register";
 	}
 
-	@PostMapping("/registerSubmit")
+	@PostMapping("/submit")
 	public String registerSubmit(@ModelAttribute("user") User user) {
 		user.setAdmin(false);
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
