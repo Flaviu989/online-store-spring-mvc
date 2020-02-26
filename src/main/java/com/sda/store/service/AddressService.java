3e63da35901cd2b1ef827920c867d124740d4a68
Flaviu989 <madomeg@yahoo.com>
2020-02-27 01:32:01 +0200

    Update Address Service

--- src/main/java/com/sda/store/service/AddressService.java
+++ src/main/java/com/sda/store/service/AddressService.java
@@ -1,5 +1,9 @@
 package com.sda.store.service;
 
+import com.sda.store.model.Address;
+
 public interface AddressService {
+
+	void saveAddress(Address address);
 
 }
--- src/main/java/com/sda/store/service/implementation/AddressServiceImpl.java
+++ src/main/java/com/sda/store/service/implementation/AddressServiceImpl.java
@@ -3,6 +3,7 @@ package com.sda.store.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sda.store.model.Address;
 import com.sda.store.repository.AddressRepository;
 import com.sda.store.service.AddressService;
 
@@ -11,5 +12,10 @@ public class AddressServiceImpl implements AddressService {
 
 	@Autowired
 	private AddressRepository addressRepository;
+
+	@Override
+	public void saveAddress(Address address) {
+		addressRepository.save(address);
+	}
 
 }
