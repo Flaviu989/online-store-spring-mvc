bfd11f503b1850146033a59bdf4925ac650525be
Flaviu989 <madomeg@yahoo.com>
2020-02-27 01:33:41 +0200

    Update Status Service

--- src/main/java/com/sda/store/service/StatusService.java
+++ src/main/java/com/sda/store/service/StatusService.java
@@ -1,5 +1,9 @@
 package com.sda.store.service;
 
+import com.sda.store.model.Status;
+
 public interface StatusService {
+
+	Status findStatus(int id);
 
 }
--- src/main/java/com/sda/store/service/implementation/StatusServiceImpl.java
+++ src/main/java/com/sda/store/service/implementation/StatusServiceImpl.java
@@ -3,6 +3,7 @@ package com.sda.store.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sda.store.model.Status;
 import com.sda.store.repository.StatusRepository;
 import com.sda.store.service.StatusService;
 
@@ -11,4 +12,9 @@ public class StatusServiceImpl implements StatusService {
 
 	@Autowired
 	private StatusRepository statusRepository;
+
+	@Override
+	public Status findStatus(int id) {
+		return statusRepository.findById(id).orElse(null);
+	}
 }
